a7b76aebbbefdd4403a718bea2534ef6752a3858
ed <ed@theworkingparty.com.au>
2014-09-01 21:54:40 +1000

    monday night

--- jekyll-portfolio/_site/assets/scripts.js
+++ jekyll-portfolio/_site/assets/scripts.js
@@ -4,3 +4,14 @@ $('#mobileMenuButton').click(function(){
 	$('body').toggleClass('menuOpen');
 });
 
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		helpers : {
+			overlay : {
+				css : {
+					'background' : 'rgba(58, 42, 45, 0.95)'
+				}
+			}
+		}
+	});
+});
